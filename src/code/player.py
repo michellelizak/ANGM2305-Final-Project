243b8371404a21b755f00020d402408f280e00b2
jhope <mchelleliz4k@gmail.com>
2023-11-23 10:36:19 -0600

    removed laser code from player

--- src/code/player.py
+++ src/code/player.py
@@ -1,5 +1,4 @@
 import pygame 
-from laser import Laser
 
 class Player(pygame.sprite.Sprite):
 	def __init__(self,pos,constraint,speed):
@@ -12,10 +11,6 @@ class Player(pygame.sprite.Sprite):
 		self.laser_time = 0
 		self.laser_cooldown = 600
 
-		self.lasers = pygame.sprite.Group()
-
-		self.laser_sound = pygame.mixer.Sound('../audio/laser.wav')
-		self.laser_sound.set_volume(0.5)
 	def get_input(self):
 		keys = pygame.key.get_pressed()
 
@@ -24,11 +19,6 @@ class Player(pygame.sprite.Sprite):
 		elif keys[pygame.K_LEFT]:
 			self.rect.x -= self.speed
 
-		if keys[pygame.K_SPACE] and self.ready:
-			self.shoot_laser()
-			self.ready = False
-			self.laser_time = pygame.time.get_ticks()
-			self.laser_sound.play()
 
 
 	def recharge(self):
@@ -42,23 +32,19 @@ class Player(pygame.sprite.Sprite):
 		if self.rect.right >= self.max_x_constraint:
 			self.rect.right = self.max_x_constraint
 
-	def shoot_laser(self):
-		self.lasers.add(Laser(self.rect.center,-8,self.rect.bottom))
-
 	def update(self):
 		self.get_input()
 		self.constraint()
 		self.recharge()
-		self.lasers.update()
+
 	def constraint(self):
 		if self.rect.left <= 0:
 			self.rect.left = 0
 		if self.rect.right >= self.max_x_constraint:
 			self.rect.right = self.max_x_constraint
-	def shoot_laser(self):
-		self.lasers.add(Laser(self.rect.center,-8,self.rect.bottom))
+
 	def update(self):
 		self.get_input()
 		self.constraint()
 		self.recharge()
-		self.lasers.update()
+
