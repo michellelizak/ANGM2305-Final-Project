f7e9a0809d6eaab5610d8f8542d7e6d21e47ae04
jhope <mchelleliz4k@gmail.com>
2023-11-23 09:19:55 -0600

    added input handling method

--- src/code/player.py
+++ src/code/player.py
@@ -16,3 +16,17 @@ class Player(pygame.sprite.Sprite):
 
 		self.laser_sound = pygame.mixer.Sound('../audio/laser.wav')
 		self.laser_sound.set_volume(0.5)
+	def get_input(self):
+		keys = pygame.key.get_pressed()
+
+		if keys[pygame.K_RIGHT]:
+			self.rect.x += self.speed
+		elif keys[pygame.K_LEFT]:
+			self.rect.x -= self.speed
+
+		if keys[pygame.K_SPACE] and self.ready:
+			self.shoot_laser()
+			self.ready = False
+			self.laser_time = pygame.time.get_ticks()
+			self.laser_sound.play()
+
