7843275f90282ed851b0a9219910934eaafa5650
jhope <mchelleliz4k@gmail.com>
2023-11-27 12:01:26 -0600

    added x coordinate positions to project.py

--- src/code/project.py
+++ src/code/project.py
@@ -13,19 +13,21 @@ class Game:
 		self.shape = obstacle.shape
 		self.block_size = 6
 		self.blocks = pygame.sprite.Group()
-		self.create_multiple_obstacles(0,480,(0,100,200))
+		self.obstacle_amount = 4
+		self.obstacle_x_positions = 
+		self.create_multiple_obstacles(0,100,200, x_start = 0, y_start = 480)
 
-	def create_obstacle(self, x_start, y_start, offset_x):
+	def create_obstacle(self,x_start,y_start,offset_x):
 		for row_index, row in enumerate(self.shape):
 			for col_index,col in enumerate(row):
 				if col == 'x':
-					x = x_start + col_index * self.block_size
-					y = y_start + col_index * self.block_size
+					x = x_start + col_index * self.block_size +  offset_x
+					y = y_start + col_index * self.block_size 
 					block = obstacle.Block(self.block_size,(241,79,80),x,y)
 					self.blocks.add(block)
 
-	def create_multiple_obstacles(self, x_start, y_start,*offset):
-		for x in offset:
+	def create_multiple_obstacles(self,*offset,x_start, y_start):
+		for offset_x in offset:
 			self.create_obstacle(x_start,y_start,offset_x)
 
 
