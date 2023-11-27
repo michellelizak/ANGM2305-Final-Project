4f7c3e95f47a5d407d53f756cd087a522b89e7c2
jhope <mchelleliz4k@gmail.com>
2023-11-27 11:27:46 -0600

    added shape variable to to create obstacle pattern

--- src/code/obstacle.py
+++ src/code/obstacle.py
@@ -6,3 +6,12 @@ class Block(pygame.sprite.Sprite):
         self.image = pygame.Surface((size,size))
         self.image.fill(color)
         self.rect = self.image.get_rect(topleft = (x,y))
+
+shape = [
+'  xxxxxxx',
+' xxxxxxxxx',
+'xxxxxxxxxxx',
+'xxxxxxxxxxx',
+'xxxxxxxxxxx',
+'xxx     xxx',
+'xx       xx']
