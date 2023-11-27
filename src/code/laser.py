262ec325a963905580bf57da071482b8af0a5cd4
jhope <mchelleliz4k@gmail.com>
2023-11-26 21:26:28 -0600

    adjusted laser class in laser.py

--- src/code/laser.py
+++ src/code/laser.py
@@ -1,17 +1,15 @@
 import pygame 
 from laser import Laser
 
-class Player(pygame.sprite.Sprite):
-    def __init__(self,pos,constraint,speed):
-
-    def get_input(self):
-
-    def recharge(self):
-
-    def constraint(self):
-
-    def shoot_laser(self):
-        self.lasers.add(Laser(self.rect.center))
+class Laser(pygame.sprite.Sprite):
+    def __init__(self,pos,speed):
+        super().__init__()
+        self.image = pygame.Surface((4,20))
+        self.image.fill('white')
+        self.rect = self.image.get_rect(center = pos)
+        self.speed = speed
 
     def update(self):
+        self.rect.y -= self.speed
+        
 
