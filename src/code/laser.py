f94dad5dbd767633d453038f078cadfc53da4191
jhope <mchelleliz4k@gmail.com>
2023-11-29 12:27:51 -0600

    added misser screen_height parameter

--- src/code/laser.py
+++ src/code/laser.py
@@ -2,14 +2,18 @@ import pygame
 from laser import Laser
 
 class Laser(pygame.sprite.Sprite):
-    def __init__(self,pos,speed):
-        super().__init__()
-        self.image = pygame.Surface((4,20))
-        self.image.fill('white')
-        self.rect = self.image.get_rect(center = pos)
-        self.speed = speed
+	def __init__(self,pos,speed,screen_height):
+		super().__init__()
+		self.image = pygame.Surface((4,20))
+		self.image.fill('white')
+		self.rect = self.image.get_rect(center = pos)
+		self.speed = speed
+		self.height_y_constraint = screen_height
 
-    def update(self):
-        self.rect.y -= self.speed
-        
+	def destroy(self):
+		if self.rect.y <= -50 or self.rect.y >= self.height_y_constraint + 50:
+			self.kill()
 
+	def update(self):
+		self.rect.y += self.speed
+		self.destroy()
