838dcc1a8a05b9caff83a42e7e1b8fcd7479cb81
jhope <mchelleliz4k@gmail.com>
2023-11-29 18:48:55 -0600

    added shoot laser function

--- src/code/player.py
+++ src/code/player.py
@@ -1,4 +1,5 @@
 import pygame 
+from laser import Laser
 
 class Player(pygame.sprite.Sprite):
 	def __init__(self,pos,constraint,speed):
@@ -10,6 +11,8 @@ class Player(pygame.sprite.Sprite):
 		self.ready = True
 		self.laser_time = 0
 		self.laser_cooldown = 600
+
+		self.lasers = pygame.sprite.Group()
 
 	def get_input(self):
 		keys = pygame.key.get_pressed()
@@ -37,8 +40,11 @@ class Player(pygame.sprite.Sprite):
 		if self.rect.right >= self.max_x_constraint:
 			self.rect.right = self.max_x_constraint
 
+	def shoot_laser(self):
+		self.lasers.add(Laser(self.rect.center,-8,self.rect.bottom))
+
 	def update(self):
 		self.get_input()
 		self.constraint()
 		self.recharge()
-
+		self.lasers.update()
