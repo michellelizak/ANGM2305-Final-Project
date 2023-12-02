9a76fe9b9ec49dd309c7d7be2bc3312c90ac27aa
jhope <mchelleliz4k@gmail.com>
2023-12-02 09:57:25 -0600

    adjusted length of the laser

--- src/code/laser.py
+++ src/code/laser.py
@@ -4,7 +4,8 @@ class Laser(pygame.sprite.Sprite):
 	def __init__(self,pos,speed,screen_height):
 		super().__init__()
 		self.image = pygame.Surface((4,20))
-		self.image.fill('white')
+		self.length = 20
+		self.image.fill('green')
 		self.rect = self.image.get_rect(center = pos)
 		self.speed = speed
 		self.height_y_constraint = screen_height
