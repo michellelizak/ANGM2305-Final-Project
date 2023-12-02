d3f0b879154caf6239fadc844b2b60aad1354313
jhope <mchelleliz4k@gmail.com>
2023-12-02 10:16:31 -0600

    cleared graident function

--- src/code/laser.py
+++ src/code/laser.py
@@ -5,7 +5,7 @@ class Laser(pygame.sprite.Sprite):
 		super().__init__()
 		self.image = pygame.Surface((4,20))
 		self.length = 20
-		self.image.fill('green')
+		self.image = self.create_gradient_surface(4, 20)
 		self.rect = self.image.get_rect(center = pos)
 		self.speed = speed
 		self.height_y_constraint = screen_height
@@ -18,11 +18,4 @@ class Laser(pygame.sprite.Sprite):
 		self.rect.y += self.speed
 		self.destroy()
 
-	def create_gradient_surface(self,length):
-		gradient_surface = pygame.Surface((4, length), pygame.SRCALPHA)
-
-		for i in range(length):
-			alpha = int(255 * (1 - i / length)) 
-			pygame.draw.line(gradient_surface, (0, 255, 0, alpha), (2, i), (2, i + 1))
-
-		return gradient_surface
+	def create_gradient_surface(self, width, length):
