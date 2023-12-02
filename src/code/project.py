2f42ec2a4fbb80deb6a4274cfcbb9d526cb6f8f0
jhope <mchelleliz4k@gmail.com>
2023-12-02 09:38:01 -0600

    attempted to add main function

--- src/code/project.py
+++ src/code/project.py
@@ -9,7 +9,10 @@ import math
 
 class Game:
 	
-	def __init__(self):
+	def __init__(self, screen_width, screen_height, screen):
+
+		self.screen = screen
+
 		# Player setup
 		player_sprite = Player((screen_width / 2,screen_height),screen_width,5)
 		self.player = pygame.sprite.GroupSingle(player_sprite)
@@ -256,29 +259,30 @@ class Game:
 		#draw sprite groups
 
 
+def main():
+    pygame.init()
+    screen_width = 600
+    screen_height = 600
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    clock = pygame.time.Clock()
+    game = Game(screen_width, screen_height, screen)
+
+    ALIENLASER = pygame.USEREVENT + 1
+    pygame.time.set_timer(ALIENLASER, 800)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == ALIENLASER:
+                game.alien_shoot()
+
+        screen.fill((30, 30, 30))
+        game.run()
+
+        pygame.display.flip()
+        clock.tick(60)
+
 if __name__ == '__main__':
-	pygame.init()
-	screen_width = 600
-	screen_height = 600
-	screen = pygame.display.set_mode((screen_width,screen_height))
-	clock = pygame.time.Clock()
-	game = Game()
-
-
-
-	ALIENLASER = pygame.USEREVENT + 1
-	pygame.time.set_timer(ALIENLASER,800)
-
-	while True:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				pygame.quit()
-				sys.exit()
-			if event.type == ALIENLASER:
-				game.alien_shoot()
-
-		screen.fill((30,30,30))
-		game.run()
-			
-		pygame.display.flip()
-		clock.tick(60)
+    main()
